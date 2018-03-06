38f53d7560bdaa7992d3225cdeb052441fbaf1fe
panpana <867159647@qq.com>
2018-03-06 11:18:21 +0800

    create app

---
+++ src/TodoInput.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import './TodoInput.css'
+
+export default class TodoInput extends Component{
+    render(){
+        return <input type="text" className="TodoInput" 
+                value={this.props.content} 
+                onKeyPress={this.submit.bind(this)}
+                onChange={this.changeTitle.bind(this)}/>
+    }
+    submit(e){
+            if (e.key === 'Enter') {
+            this.props.onSubmit(e)
+            console.log("按回车啦")
+        }
+    }
+    changeTitle(e){
+          this.props.onChange(e)
+    }
+}
---
+++ src/Welcome.js
@@ -0,0 +1,68 @@
+import React from 'react'; 
+class Welcome extends React.Component {
+  constructor(props){
+      super(props)
+      this.state = {
+        date: new Date(),
+        test: '1'
+      }
+       setInterval(()=>{
+        this.setState({
+            date: new Date(),
+            test: 'constructor'
+        })
+     },1000)
+
+    console.log("我应经在 constructor 里将 props 和 state 初始化好了")
+  }
+  conponentWillMount(){
+    this.setState({
+              date: new Date(), // 更新 date
+              test: 'componentWillMount'
+            })
+      console.log("运行到这里的话，说明马上就要运行 render 了~")
+  }
+
+  render(){
+
+      console.log("嗯，这里是 render。")
+      return (
+          <div>
+              <h1>Hello,{this.props.name}</h1>
+              <h2>{this.state.date.toLocaleTimeString()}</h2>
+          </div>
+      )
+  }
+  componentDidMount(){
+    this.setState({
+              date: new Date(), // 更新 date
+              test: 'componentDidMount'
+            })
+      console.log("已经挂在到页面里啦")
+  }
+  componentWillReceiveProps(){
+      this.setState({
+          date: new Date(),
+          test: 'componentWillReceiveProps'
+      })
+  }
+  shouldComponentUpdate(){
+        // this.setState({
+        //     date: new Date(),
+        //     test: 'shouldComponentUpdate'
+        // })
+        return true
+    }  
+   componentWillUpdate(){
+       
+    }  
+   componentDidUpdate(){
+        
+    }  
+   componentWillUnmount(){
+    console.log('要死了')
+
+    }
+}
+
+export default Welcome 
